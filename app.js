ed5db3e28400431fe618527769040683bc92c3da
Taylor Parrish <twpearish@gmail.com>
2018-03-06 17:47:08 +0800

    simple node and stripe app

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const keys = require('./config/keys');
+const stripe = require('stripe')('keys.stripeSecretKey');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+// Body Parser MiddleWare
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set Static Folder
+app.use(express.static(`${__dirname}/public`));
+
+// Index Route
+app.get('/', (req,res) => {
+	res.render('index');
+})
+
+app.get('/success', (req,res) => {
+	res.render('success');
+})
+
+// Chare Route
+app.post('/charge', (req, res) => {
+	const amount = 2500;
+
+	stripe.customers.create({
+		email: req.body.stripeEmail,
+		source: req.body.stripeToken
+	})
+
+	.then(customer => stripe.charges.create({
+		amount,
+		description:'Dat Ass',
+		currency: 'usd',
+		customer: customer.id
+	}))
+
+	.then(charge => res.render('success'));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+	console.log(`server started on port ${port}`);
+});
+
